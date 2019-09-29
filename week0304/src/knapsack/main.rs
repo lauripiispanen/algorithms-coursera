dede5e63e608eb008aac9ab3b8642f03e57cb832
Lauri Piispanen <lauri.piispanen@reaktor.com>
2019-09-29 21:10:23 +0300

    fix bug in knapsack algorithm

--- week0304/src/knapsack/main.rs
+++ week0304/src/knapsack/main.rs
@@ -17,7 +17,7 @@ fn main() {
     let input_file = arg_matches.value_of("INPUT_FILE").unwrap();
 
     let mut i = read_numeric_lines::<u32>(input_file).map(|v| (v[0], v[1]));
-    let (knapsack_size, num_items):(usize, usize) =
+    let (knapsack_size, _):(usize, usize) =
             i.next()
              .map(|(a, b)| (
                  a.try_into().unwrap(),
@@ -26,27 +26,50 @@ fn main() {
 
     let input = i.collect::<Vec<(u32, u32)>>();
 
+    println!("{}", knapsack_value(input, knapsack_size));
+}
+
+type Value = u32;
+type Weight = u32;
+
+fn knapsack_value(input:Vec<(Value, Weight)>, knapsack_size: usize) -> u32 {
+    let num_items = input.len();
     let mut output:Vec<Vec<u32>> = Vec::with_capacity(num_items);
 
-    output.push(vec![0; knapsack_size]);
+    output.push(vec![0; knapsack_size + 1]);
     for i in 1..=num_items {
         output.push(Vec::with_capacity(num_items));
-        let input_weight:usize = input[i - 1].0.try_into().unwrap();
-        let input_value:u32 = input[i - 1].1;
-        for x in 0..knapsack_size {
+        let input_value:u32 = input[i - 1].0;
+        let input_weight:usize = input[i - 1].1.try_into().unwrap();
+        for x in 0..=knapsack_size {
             let a:u32 = output[i - 1][x];
             if (x as i64 - input_weight as i64) < 0 {
                 output[i].push(a);
             } else {
-                let b:u32 = output[i - 1][x - input_weight];
+                let b:u32 = output[i - 1][x - input_weight] + input_value;
                 if a > b {
                     output[i].push(a);
                 } else {
-                    output[i].push(b + input_value);
+                    output[i].push(b);
                 }
             }
         }
     }
 
-    println!("{}", output[num_items][knapsack_size - 1]);
+    return output[num_items][knapsack_size];
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_knapsack() {
+        let mut v = vec![];
+        v.push((3, 4));
+        v.push((2, 3));
+        v.push((4, 2));
+        v.push((4, 3));
+
+        assert_eq!(8, super::knapsack_value(v, 6))
+    }
 }
